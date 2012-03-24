2dfb230a5d9613f88a7169ad68973270b88c4437
Joshua Thijssen <jthijssen@noxlogic.nl>
2012-03-24 12:15:25 +0100

    Added better example

--- example/example.php
+++ example/example.php
@@ -5,26 +5,49 @@
 
 // Initialize shout object
 $shout = new Shout();
+
+// Protocol information where to connect to
 $shout->set_protocol(shout::PROTOCOL_HTTP);
 $shout->set_host("localhost");
 $shout->set_port("8000");
-$shout->set_mount("/listen.m3u");
+$shout->set_mount("/listen");
+
+// Authentication
 $shout->set_user("source");
 $shout->set_password("secret");
+
+// Stream type
 $shout->set_format(shout::FORMAT_MP3);
 
-// Open stream
+// Stream detail information
+$shout->set_name("My super awesome music stream");
+$shout->set_url("http://www.adayinthelifeof.nl");
+$shout->set_description("A description of the stream");
+
+// Additional audio information
+$shout->set_audio_info($shout::AI_BITRATE, 320);
+$shout->set_audio_info($shout::AI_SAMPLERATE, 44100);
+$shout->set_audio_info($shout::AI_CHANNELS, 2);
+
+
+// Open the stream
 if ($shout->open() != shout::ERR_SUCCESS) {
   die ("Cannot open stream to server");
 }
 
+// Open MP3 file to stream
 $f = fopen("file.mp3", "r");
 if ($f == null) {
   die ("Cannot open MP3 file");
 }
 
-// Repeat until the end of file, or the stream closed
+// Set the author and song title like: "<artist> - <title>"
+$meta = array("song" => "The Artist - A title - An Album");
+$shout->set_metadata($meta);
+
+// Repeat until the end of file, or until the stream has closed
 while (!feof($f) && $shout->get_connected() == shout::ERR_CONNECTED) {
+  print ".";
 
   // Read data from file
   $data = fread($f, 4096);
@@ -36,9 +59,8 @@ while (!feof($f) && $shout->get_connected() == shout::ERR_CONNECTED) {
   $shout->sync();
 }
 
-// Finialize
+// Finalize
 fclose($f);
 $shout->close();
-
 
 ?>
